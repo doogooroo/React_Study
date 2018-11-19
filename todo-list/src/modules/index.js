d07dd71d9901c9913a0423c5a37472f6b70e01b5
doogooroo_nci <doogooroo87@gmail.com>
2018-11-19 18:18:31 +0900

    1119

---
+++ todo-list/src/modules/index.js
@@ -0,0 +1,6 @@
+import input from './input';
+import todos from './todos';
+import { combineReducers } from 'redux';
+
+export default combineReducers ({ input, todos});
+
--- todo-list/src/modules/todos.js
+++ todo-list/src/modules/todos.js
@@ -15,4 +15,18 @@ export const remove = createAction(REMOVE);
 
 // 리듀서 초기값 설정
 const initialState = List( [ Map({ id: 0, text: 'Study React', done:true})
-                            , Map({ id:1, text: 'Component Styling', done:false })])
+                            , Map({ id:1, text: 'Component Styling', done:false })]);
+
+export default handleActions({ 
+    [INSERT] : (state, action) => {
+        const { id, text, done} = action.payload;
+        return state.push(Map({ id, text, done}));} ,
+    [TOGGLE] : (state, action) => {
+        const {payload : index} = action; // = const index = action.payload;
+        return state.updateIn([index, 'done'], done => !done);}, 
+    [REMOVE] : (state, action) => {
+        const { payload : index} = action;
+        return state.delete(index);}
+}, initialState)
+
+                            
